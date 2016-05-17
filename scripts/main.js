845bd11d792e54a4304f0c11908b51c81e27f566
wizard2014 <wizard2014@ya.ru>
2016-05-17 23:11:51 +0300

    complete inventory form

--- scripts/main.js
+++ scripts/main.js
@@ -252,6 +252,18 @@ var App = React.createClass({
  		return (
  			<div className="fish-edit" key={key}>
  				<input type="text" valueLink={linkState('fishes.' + key + '.name')} />
+ 				<input type="text" valueLink={linkState('fishes.' + key + '.price')} />
+
+ 				<select valueLink={linkState('fishes.' + key + '.status')}>
+ 					<option value="unavailable">Sold Out!</option>
+ 					<option value="available">Fresh!</option>
+ 				</select>
+
+ 				<textarea valueLink={linkState('fishes.' + key + '.desc')}></textarea>
+
+ 				<input type="text" valueLink={linkState('fishes.' + key + '.image')} />
+
+ 				<button>Remove Fish</button>
  			</div>
 		);
  	},
